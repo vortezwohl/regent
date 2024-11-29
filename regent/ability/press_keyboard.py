6671d3ef762d76150eb76f67c8ec305388dc5cfa
吴子豪 <vortezwohl@proton.me>
2024-11-30 02:18:59 +0800

    调整键盘控制能力

--- regent/ability/press_keyboard.py
+++ regent/ability/press_keyboard.py
@@ -12,37 +12,32 @@ def press_key(key: str) -> bool:
 
 @ability
 def press_key_with_another_one_key_held_down(key: str, pre_held_key: str) -> bool:
-    pyautogui.hotkey(pre_held_key, key)
+    pyautogui.hotkey(pre_held_key.lower(), key.lower())
     return True
 
 
 @ability
 def press_key_with_another_two_keys_held_down(key: str, pre_held_key1: str, pre_held_key2: str) -> bool:
-    pyautogui.hotkey(pre_held_key1, pre_held_key2, key)
+    pyautogui.hotkey(pre_held_key1.lower(), pre_held_key2.lower(), key.lower())
     return True
 
 
 @ability
-def type_input_text(text: str) -> bool:
+def write_text_with_keyboard(text: str) -> bool:
     pyautogui.write(text)
     return True
 
 
-additional_info_key = json.dumps({
+additional_info = json.dumps({
     'keys_available': pyautogui.KEYBOARD_KEYS
 }, ensure_ascii=False)
 
-additional_info_hotkey = json.dumps({
-    'keys_available': pyautogui.KEYBOARD_KEYS,
-    'hint_for_hotkey_presses': 'Carefully think if user really wants a hotkey, '
-                               'and seriously think the order of your operations.'
-}, ensure_ascii=False)
-
 additional_info_type_input_text = json.dumps({
-    'hint_for_type_input_text': 'If you want to input more than one key, you want to input a string or text, use this function.'
+    'hint_for_type_input_text': 'If you want to input a string or text '
+                                'instead of press a single key, use this function.'
 }, ensure_ascii=False)
 
-press_key.__doc__.join(additional_info_key)
-press_key_with_another_one_key_held_down.__doc__.join(additional_info_hotkey)
-press_key_with_another_two_keys_held_down.__doc__.join(additional_info_hotkey)
-type_input_text.__doc__.join(additional_info_type_input_text)
+press_key.__doc__.join(additional_info)
+press_key_with_another_one_key_held_down.__doc__.join(additional_info)
+press_key_with_another_two_keys_held_down.__doc__.join(additional_info)
+write_text_with_keyboard.__doc__.join(additional_info_type_input_text)
--- test/press_keyboard.py
+++ test/press_keyboard.py
@@ -1,23 +1,23 @@
 import logging
 
 from ceo import Agent, get_openai_model
-
 from regent.ability.press_keyboard import (
     press_key,
     press_key_with_another_one_key_held_down,
     press_key_with_another_two_keys_held_down,
-    type_input_text
+    write_text_with_keyboard
 )
 
 logging.getLogger('ceo').setLevel(logging.DEBUG)
+
 
 if __name__ == '__main__':
     agent = Agent([
         press_key,
         press_key_with_another_one_key_held_down,
         press_key_with_another_two_keys_held_down,
-        type_input_text
+        write_text_with_keyboard
     ],
         get_openai_model(), 'test')
-    agent.assign('输入 win + r，然后 cmd，然后回车，然后输入dir，然后回车')
+    agent.assign('输入五个enter，然后输入"nb"')
     agent.just_do_it()
