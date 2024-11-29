2a79f27d420dc515fb6507105d9288a9b73707b3
吴子豪 <vortezwohl@proton.me>
2024-11-29 23:57:49 +0800

    调整键盘控制能力

--- regent/ability/press_keyboard.py
+++ regent/ability/press_keyboard.py
@@ -5,27 +5,28 @@ from ceo.util import ability
 
 
 @ability
-def press_a_key_with_another_one_key_held_down(key: str, pre_held_key: str) -> bool:
+def press_key_with_another_one_key_held_down(key: str, pre_held_key: str) -> bool:
     pyautogui.hotkey(pre_held_key, key)
     return True
 
 
 @ability
-def press_a_key_with_another_two_keys_held_down(key: str, pre_held_key1: str, pre_held_key2: str) -> bool:
+def press_key_with_another_two_keys_held_down(key: str, pre_held_key1: str, pre_held_key2: str) -> bool:
     pyautogui.hotkey(pre_held_key1, pre_held_key2, key)
     return True
 
 
 @ability
-def type_a_text(text: str) -> bool:
+def type_text(text: str) -> bool:
     pyautogui.write(text)
     return True
 
 
 additional_info = json.dumps({
     'keys_available': pyautogui.KEYBOARD_KEYS,
-    'hint_for_hotkey_presses': 'Carefully think if user really wants a hotkey.'
+    'hint_for_hotkey_presses': 'Carefully think if user really wants a hotkey, '
+                               'and seriously think the '
 }, ensure_ascii=False)
 
-press_a_key_with_another_one_key_held_down.__doc__.join(additional_info)
-press_a_key_with_another_two_keys_held_down.__doc__.join(additional_info)
+press_key_with_another_one_key_held_down.__doc__.join(additional_info)
+press_key_with_another_two_keys_held_down.__doc__.join(additional_info)
--- test/press_keyboard.py
+++ test/press_keyboard.py
@@ -3,19 +3,19 @@ import logging
 from ceo import Agent, get_openai_model
 
 from regent.ability.press_keyboard import (
-    press_a_key_with_another_one_key_held_down,
-    press_a_key_with_another_two_keys_held_down,
-    type_a_text
+    press_key_with_another_one_key_held_down,
+    press_key_with_another_two_keys_held_down,
+    type_text
 )
 
 logging.getLogger('ceo').setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
     agent = Agent([
-        press_a_key_with_another_one_key_held_down,
-        press_a_key_with_another_two_keys_held_down,
-        type_a_text
+        press_key_with_another_one_key_held_down,
+        press_key_with_another_two_keys_held_down,
+        type_text
     ],
         get_openai_model(), 'test')
-    agent.assign('键入 "abcd"')
+    agent.assign('键入 abcd, 然后 ctrl + a')
     agent.just_do_it()
