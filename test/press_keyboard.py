1367c7432539b3500deef08fb1aaa804ce6ca4ab
吴子豪 <vortezwohl@proton.me>
2024-12-03 21:24:31 +0800

    update

--- test/press_keyboard.py
+++ test/press_keyboard.py
@@ -14,5 +14,5 @@ abilities = [press_key, press_key_with_another_one_key_held_down,
 
 if __name__ == '__main__':
     agent = Agent(abilities=abilities, brain=get_openai_model(), name='test', personality=Personality.PRUDENT)
-    agent.assign('按enter; 然后输入nb; 再按enter; 输入222')
+    agent.assign('press enter, type "hello world", then press enter again and type "this is reg"')
     agent.just_do_it()
